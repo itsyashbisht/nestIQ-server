3486c46fcbfe1dc2cb87d1379087d79c1e3ef4b2
itsyashbisht <yashbisht2005@gmail.com>
2026-03-28 12:21:37 +0530

    update room.routes.js : fixed upload using multer middleware, also created the multer middleware

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -10,17 +10,34 @@ import {
   toggleRoomAvailability,
   updateRoom,
 } from "../controllers/room.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/create").post(createRoom);
+router.route("/create").post(
+  createRoom,
+  upload.fields([
+    {
+      name: "images",
+      maxCount: 10,
+    },
+  ]),
+);
 router.route("/delete/:roomId").delete(deleteRoom);
 router.route("/update/:roomId").patch(updateRoom);
 router.route("/:roomId").get(getRoomById);
 router.route("/hotel/:hotelId").get(getRoomsByHotel);
 router.route("/:roomId/toggle-room-availability").put(toggleRoomAvailability);
-router.route("/:roomId/add-images").post(addRoomImages);
+router.route("/:roomId/add-images").post(
+  addRoomImages,
+  upload.fields([
+    {
+      name: "images",
+      maxCount: 10,
+    },
+  ]),
+);
 router.route("/:roomId/remove-images").delete(removeRoomImage);
 
 export default router;
