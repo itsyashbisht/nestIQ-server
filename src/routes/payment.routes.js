b22390278b9dd6da0793b70a8f9af5e824ab3ea3
itsyashbisht <yashbisht2005@gmail.com>
2026-05-03 21:21:02 +0530

    fix: change payment verification endpoint from GET to POST

--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
-import {
-  getRazorpayKey,
-  verifyPayment,
-} from "../controllers/payment.controllers.js";
+import { getRazorpayKey, verifyPayment } from "../controllers/payment.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 router.use(verifyJWT);
 
-router.route("/verify").get(verifyPayment);
+router.route("/verify").post(verifyPayment);
 router.route("/razorpayKey").get(getRazorpayKey);
 
 export default router;
