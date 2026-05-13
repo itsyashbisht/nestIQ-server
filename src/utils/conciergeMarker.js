67fb273b9778be84eefcfe0072eb8aef2fff5804
itsyashbisht <yashbisht2005@gmail.com>
2026-05-13 13:06:11 +0530

    feat(concierge): add booking link marker utility

--- src/utils/conciergeMarker.js
+++ src/utils/conciergeMarker.js
@@ -2,10 +2,13 @@ function injectHotelsMarker(text, hotels) {
   const marker = `[[HOTELS:${JSON.stringify({ hotels })}]]`;
   return text + "\n" + marker;
 }
+function injectBookingLinkMarker(text, link) {
+  return `${text}\n[[BOOKING_LINK:${JSON.stringify({ link })}]]`;
+}
 
 function injectRoomsMarker(text, rooms, hotelName, hotelSlug) {
   const marker = `[[ROOMS:${JSON.stringify({ rooms, hotelName, hotelSlug })}]]`;
   return text + "\n" + marker;
 }
 
-export { injectRoomsMarker, injectHotelsMarker };
+export { injectRoomsMarker, injectHotelsMarker, injectBookingLinkMarker };
